6a735d959ef64b00d373bdb14e814420352992a3
unknown <abdulkhaev96@gmail.com>
2022-06-17 02:44:02 +0900

    main-page-updated

--- src/components/Home/Hero/hero.jsx
+++ src/components/Home/Hero/hero.jsx
@@ -1,101 +1,41 @@
 import React from 'react'
 
-
-const style = {
-    wrapper: `lg:relative flex justify-center`,
-    container: `before:content-['']  before:bg-red-500  before:absolute 
-    before:top-0 before:left-0 before:right-0 before:bottom-0
-    before:bg-[url('https://lh3.googleusercontent.com/soR6JAuB0k-X3Az9G8-NJG9Cfoc4kyfgPUCYVtp6Ker9QQSHg9UQA52eFduoL_v_E5dHnnaB3LtZjBIAZdH6p5e2rLOm7aVm6eVoMg=w600')]
-    before:bg-cover before:bg-bottom before:opacity-20 before:blur
-    lg:h-[36rem]
-`,
-    contentWrapper: `
-    flex justify-center  
-    lg:flex h-screen relative flex-wrap
-    `,
-    contentBodyWrapper:`
-    lg:flex lg:mt-[3rem] lg:justify-evenly 
-   `,
-    contentWrapperMini:`
-    lg:flex  lg:h-2/4  flex justify-center items-center`,
-    copyContainer: `
-    flex-col w-1/2 justify-center mt-[2rem] 
-    lg:w-3/4
-    `,
-    title: `
-    relative text-black text-[30px] text-center lg:text-left font-semibold
-    lg:text-[46px] lg:text-left
-    `,
-    description: `text-[rgb(53, 56, 64)] lg:w-[25rem] text-center text-2xl mt-[0.2rem] mb-[1rem]
-    lg:text-left`,
-    ctaContainer: `flex justify-between ml-[-1rem] lg:w-[25rem]`,
-    accentedButton: `
-    px-8 py-1
-     relative text-lg font-semibold lg:px-12 lg:py-2 bg-[#2181e2]  rounded-lg text-white   hover:bg-[#42a0ff] shadow hover:shadow-xl cursor-pointer`,
-    button: `
-    px-8 py-1
-    relative text-lg font-semibold lg:px-12 lg:py-2 bg-white ml-5 rounded-lg text-[#2181e2] border-solid border-2 border-sky-500 shadow hover:shadow-xl cursor-pointer`,
-    cardContainer: `flex-col
-    hover hover:shadow-xl cursor-pointer shadow-xl
-    h-[22rem] w-[20rem]
-    lg:w-[30rem] lg:h-[20rem]
-   `,
-   img:`rounded-t-lg h-[18rem] w-[20rem] lg:w-[30rem] lg:h-[20rem] `,
-    infoContainer: `h-[4rem] bg-white rounded-b-lg flex p-3 text-white font-semibold
-    lg:h-20`,
-    author: `flex flex-col text-black ml-4`,
-    name: ``,
-    imgWrapper:`flex justify-center mt-[3rem] h-fit`,
-  }
-
-
 const Hero = () => {
-  return (
-  
-    <div className={style.wrapper}>
-        <div className={style.container}>
-            <div className={style.contentWrapper}>
-                <div className={style.contentBodyWrapper}>
-                <div className={style.contentWrapperMini}>
-                <div className={style.copyContainer}>
-                    <div className={style.title}>
-                    Discover, collect, and sell extraordinary NFTs
+    return (
+
+        <div className="before:content-['']  before:bg-red-500  before:absolute 
+       before:top-0 before:left-0 before:right-0 before:bottom-0
+       before:bg-[url('https://lh3.googleusercontent.com/soR6JAuB0k-X3Az9G8-NJG9Cfoc4kyfgPUCYVtp6Ker9QQSHg9UQA52eFduoL_v_E5dHnnaB3LtZjBIAZdH6p5e2rLOm7aVm6eVoMg=w600')]
+       before:bg-cover before:bg-bottom before:opacity-20 before:blur
+       lg:h-[36rem]">
+            <div className='flex flex-col items-center mt-10'>
+                <h1 className='text-5xl font-bold'>Discover paint, collect, and sell extraordinary NFTs</h1>
+                <p className='text-2xl m-4'>YU NFT market is made by Yeungnam University students.</p>
+                <div className='flex gap-10 mt-5'>
+                    <div className='flex flex-col'>
+
+                        <div className='border w-[18rem] h-[18rem] rounded-md'>
+                            <img className='h-[100%] flex justify-center' src="https://i.pinimg.com/originals/6c/79/ac/6c79ac1ee58b1ab90d7bbc0beab77a1a.jpg" alt="data is broken" />
+                        </div>
+                        <div className='flex cursor-pointer border mt-5 h-14 rounded-md justify-center items-center bg-slate-500 text-white font-bold'>My NFT</div>
                     </div>
-                    <div className={style.description}>
-                    OpenSea is the world's first and largest NFT marketplace
+                    <div>
+                        <div className='border w-[18rem] h-[18rem] rounded-md '>
+                            <img className='h-[100%] flex justify-center' src="https://mobcompany.info/wp-content/uploads/2019/09/02-84.jpg" alt="data is broken" />
+                        </div>
+                        <div className='flex cursor-pointer border mt-5 h-14 rounded-md justify-center items-center bg-slate-500 text-white font-bold'>Make NFT</div>
                     </div>
-                    <div className={style.ctaContainer}>
-                        <button className={style.accentedButton}>Explore</button>
-                        <button className={style.button}>Create</button>
-                    </div>
-                    </div>
-                </div>
-                <div className={style.imgWrapper}>
-                <div className={style.cardContainer}>
-                <img className={style.img}  src="https://lh3.googleusercontent.com/soR6JAuB0k-X3Az9G8-NJG9Cfoc4kyfgPUCYVtp6Ker9QQSHg9UQA52eFduoL_v_E5dHnnaB3LtZjBIAZdH6p5e2rLOm7aVm6eVoMg=w600"  alt='img'/>
-                <div className={style.infoContainer}>
-                    <img
-                    className='h-[2.25rem] rounded-full' 
-                    src='https://lh3.googleusercontent.com/XuDfRQtqu8q9sOIW2VkGokR_CEFDxybbmrNG31R8dAEboi4Co5qdn-JUwmtG0S7JYhCnj4wK5rSbi-J6giG8zTXBvE_u8I0ymhRPgA=s80'
-                    alt="cartWrap"
-                    />
-                    <div className={style.author}>
-                        <div className={style.name}>ATHENA</div>
-                        <a
-                        className='text-[#1868b7]'
-                        href="/"
-                        >
-                        animkhitaryan
-                        </a>
+                    <div>
+
+                        <div className='border w-[18rem] h-[18rem] rounded-md'>
+                            <img className='h-[100%] flex justify-center' src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs2/224431915/original/d5140f0cac6735519dcc60aa898ebb18a1c8049d/make-10-000-unique-nfts.jfif" alt="data is broken" />
+                        </div>
+                        <div className='flex cursor-pointer border mt-5 h-14 rounded-md justify-center items-center bg-slate-500 text-white font-bold'>Make NFT</div>
                     </div>
                 </div>
-                </div>
-                </div>
+            </div>
         </div>
-        </div>
-        </div>
-     </div>
-  )
+    )
 }
 
 export default Hero
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,17 +10,17 @@ const style = {
   nav: `flex `,
   dropcontainer: ``,
   container: `sticky top-0 z-10 `,
-  wrapper: ` bg-white  w-screen px-[1.1rem] py-[0.8rem] flex shadow-[0_35px_60px_-2px_rgba(0,0,0,0.2)]`,
+  wrapper: `bg-slate-700 w-screen px-[1.1rem] py-[0.8rem] flex shadow-[0_35px_60px_-2px_rgba(0,0,0,0.2)]`,
   logoContainer: `flex mr-[10rem] items-center cursor-pointer`,
-  logoText: ` ml-[0.8rem] text-black font-bold text-2xl`,
-  searchBar: `flex flex-1 mx-[0.8rem] w-max-[600px] items-center bg-[#fffff] rounded-[0.6rem] border-solid border-2 border-slate-300`,
+  logoText: ` ml-[0.8rem] text-[white] font-bold text-2xl`,
+  searchBar: `flex flex-1 mx-[0.8rem] w-max-[600px] items-center bg-[#fffff] rounded-[0.6rem] border-solid border-2 `,
   searchIcon: `text-[#8a939b] mx-3 font-bold text-lg`,
-  searchInput: `h-[2.6rem] w-full border-0 bg-transparent outline-0  ring-0 px-2 pl-0 text-[black] placeholder:text-[#8a939b]`,
+  searchInput: `h-[2.6rem] w-full border-0 bg-transparent outline-0  ring-0 px-2 pl-0 text-[white] placeholder:text-[#8a939b]`,
   headerItems: ` flex items-center justify-end`,
   headerItem: `text-white px-4 font-bold text-[#808080]  hover:text-black cursor-pointer`,
   navmaincon: ` w-[14rem] bg-white mt-3 shadow-[0_35px_60px_-2px_rgba(0,0,0,0.2)] rounded-b-lg `,
   navcon: `text-white px-4 font-bold text-[#808080]  hover:text-black cursor-pointer h-12 border-b-[1px] items-baseline  hover:shadow-[0px_0px_5px_3px_rgba(0,0,0,0.2)]`,
-  headerIcon: `text-[#8a939b] text-3xl font-black px-4 hover:text-black cursor-pointer`,
+  headerIcon: `text-white text-3xl font-black px-4 hover:text-black cursor-pointer`,
   context: `pt-3 pb-3 text-[#808080]`,
 }
 
@@ -50,7 +50,7 @@ const Navbar = () => {
               <div>
                 <Link to='/expore'>
                   <div className={style.headerItem}>
-                    <div className={style.context}>Explore</div>
+                    <div className="text-white hover:text-black">Explore</div>
                   </div>
                 </Link>
               </div>
